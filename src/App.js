1677d51f037d7525bbb7732bd903ae4c24892b27
bbosmedia <bbosmedia@users.noreply.github.com>
2020-09-23 23:50:18 +0500

    I added Movie.jsx Component to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './components/Movie';
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello World</h1>
+			<Movie />
 		</div>
 	);
 }
---
+++ src/components/Movie.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Movie = () => {
+	return (
+		<div className="movie">
+			<h2>Movie Component</h2>
+		</div>
+	);
+};
+
+export default Movie;
